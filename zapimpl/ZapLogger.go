1d9da832935c660fc11e93abef3d2dde2bdd4624
architect.bian <greatarchitect@163.com>
2018-09-12 12:04:33 +0800

    modify sugar to save as field

--- zapimpl/ZapLogger.go
+++ zapimpl/ZapLogger.go
@@ -11,7 +11,7 @@ import (
 )
 
 type Logger struct {
-	log       *zap.Logger
+	log       *zap.SugaredLogger
 	atomLevel zap.AtomicLevel
 }
 
@@ -22,10 +22,11 @@ func (z *Logger) getLog() *zap.SugaredLogger {
 		workDir, _ := filepath.Abs("./")
 		var file = filepath.Join(workDir, "conf", "log.json")
 		_, err := os.Stat(file)
+		var log *zap.Logger
 		if err != nil || os.IsNotExist(err) {
 			conf := zap.NewProductionConfig()
 			conf.Level = z.atomLevel
-			z.log, _ = conf.Build(opts...)
+			log, _ = conf.Build(opts...)
 		} else { // load config from file then create a logger
 			buf, err := ioutil.ReadFile(file)
 			if err != nil {
@@ -36,10 +37,11 @@ func (z *Logger) getLog() *zap.SugaredLogger {
 				panic(err)
 			}
 			conf.Level = z.atomLevel
-			z.log, _ = conf.Build(opts...)
+			log, _ = conf.Build(opts...)
 		}
+		z.log = log.Sugar()
 	}
-	return z.log.Sugar()
+	return z.log
 }
 
 func (z *Logger) SetLevel(lvl int8) {
